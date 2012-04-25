d9f7a3e957a6b64d6e791d1c12b4fafdfcfb7912
juleea <julia@cs.stanford.edu>
2012-04-25 00:57:57 -0700

    starter rails project

---
+++ RainbowOnion/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RainbowOnion::Application.config.secret_token = '5d4568ada9f1e53ffe938b467413d1b17cd5f2e62e97e4032569c4a626c07af5384b105ece2d8769a57986e7a8c1bef97e683f3942fbc716adbad88de1dca0d1'
