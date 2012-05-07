0c8ea1f2577127f4c071a67ff3e99d8fd5c68f01
Kat Busch <kbusch@cs.stanford.edu>
2012-05-06 21:46:01 -0700

    redone move, addede add =P untested on actualy memory

--- instructions.js
+++ instructions.js
@@ -1,13 +1,32 @@
 instructionMap = {};
 instructionMap['mov'] = Mov;
+instructionMap['add'] = Add;
+
 
 /* Mov */
 Mov.prototype.execute = function(memory, registers) {
-    alert("executed mov with params " + this.params[0] + " and " + this.params[1]);    
-    registers.setContents(this.params[1],this.params[0]);
+  alert("executed mov with params " + this.params[0] + " and " + this.params[1]);    
+  var src = this.params[0].getValue();
+  var dest = this.params[1].getValue();
+  registers.setContents(dest, registers.getContents(src));
 }
 
 function Mov(parameters) {
+  this.parameters = parameters;
+  this.valid = true;
+  if(!(this.params && this.params.length == 2))
+    this.valid = false;
+}
+
+Add.prototype.execute = function(memory, registers) {
+  alert("executed add with params " + this.params[0] + " and " + this.params[1]);    
+  var src = this.params[0].getValue();
+  var dest = this.params[1].getValue();
+  var sum = registers.getContents(src) + registers.getContents(dest);
+  registers.setContents(dest, sum);
+}
+
+function Add(parameters) {
   this.parameters = parameters;
   this.valid = true;
   if(!(this.params && this.params.length == 2))
--- memory.js
+++ memory.js
@@ -4,10 +4,9 @@ function Memory() {
   var memory_start = 0;
   var memory_end = 4096;
   //Public
-  this.init = function () {
-    
+  this.init = function () { 
   }
-  
+
   //Must give valid address!
   this.setContents = function(address, bytes, data) {
     //TODO what if not multiple of 4?
