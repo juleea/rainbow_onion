6588579533f8d0a89a46ca53fe1a2c2d50c6f381
Kat Busch <kbusch@cs.stanford.edu>
2012-06-09 17:04:56 -0700

    made rainbow rainbow and o onion

--- instrData.js
+++ instrData.js
@@ -42,3 +42,51 @@ Neg.help = "Dest = -Dest"
 
 Not.form = "not Dest"
 Not.help = "Dest = ~Dest"
+
+Lea.form = "lea Src, Dest"
+Lea.help = "Dest = address Src points to"
+
+Cmp.form = "cmp Src2, Src1"
+Cmp.help = "Set flags based on Src1-Src2"
+
+Test.form = "test Src2, Src1"
+Test.help = "Set flags based on Src1&Src2"
+
+Jmp.form = "jmp label"
+Jmp.help = "run code at label"
+
+Je.form = "je label"
+Je.help = "jump if compared values were equal (ZF on)"
+
+Jne.form = "jne label"
+Jne.help = "jump if compared values were not equal (ZF off)"
+
+Js.form = "js label"
+Js.help = "jump if comparison negative (SF on)"
+
+Jns.form = "jns label"
+Jns.help = "jump if comparison non-negative (SF off)"
+
+Jg.form = "jg label"
+Jg.help = "jump if second value greater"
+
+Jge.form = "jge label"
+Jge.help = "jump if second value greater or equal"
+
+Jle.form = "jle label"
+Jle.help = "jump if second value less"
+
+Jle.form = "jle label"
+Jle.help = "jump if second value less or equal"
+
+Ja.form = "ja label"
+Ja.help = "jump if second above first (unsigned greater)"
+
+Jb.form = "jb label"
+Jb.help = "jump if second below first (unsigned less)"
+
+Push.form = "push Src"
+Push.help = "Put contents of Src in Mem[%esp] and esp-=4"
+
+Push.form = "pop Src"
+Push.help = "Put contents of Mem[%esp] in Src and esp+=4"
