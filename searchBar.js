746155e7b6b1da3a8aa6f0e4eb0f6bdc94820940
Kat Busch <kbusch@cs.stanford.edu>
2012-06-09 17:47:08 -0700

    updated help to be at bottom and respond to typings

--- searchBar.js
+++ searchBar.js
@@ -1,4 +1,4 @@
-function createHelpBox(inputId, outputId, initialValue) {
+function HelpBox(inputId, outputId, initialValue) {
   //This is a crap hack.  Hopefully we can figure out a better way to do sizing in general
   var inputElem = $("#" + inputId);
   var outputElem = $("#" + outputId);
@@ -8,16 +8,19 @@ function createHelpBox(inputId, outputId, initialValue) {
     if(e.keyCode == 13){
       var instr = inputElem.val().toLowerCase();
       if(instr in instructionMap) {
-        outputElem.html("<b>" + instr + "</b><br />" + instructionMap[instr].form + "<br />" + instructionMap[instr].help);
+        outputElem.html("&nbsp;&nbsp;&nbsp;&nbsp; " + instructionMap[instr].form + "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;" + instructionMap[instr].help);
       } else {
         outputElem.html("Instruction not found")
       }
     }
   }
-
   inputElem.keyup(keyFunction);
   if(initialValue) {
     inputElem.val(initialValue);
+    keyFunction({"keyCode": 13});
+  }
+  this.search = function(word) {
+    inputElem.val(word);
     keyFunction({"keyCode": 13});
   }
 }
